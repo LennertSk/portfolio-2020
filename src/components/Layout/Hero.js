6f2c358d243a07675309c1f3f001578060772cf4
Lennert Slabbinck <lennert.slabbinck@hotmail.com>
2020-07-29 20:36:31 +0200

    Fixed Hero & Hero Image

--- src/components/Layout/Hero.js
+++ src/components/Layout/Hero.js
@@ -1,37 +1,46 @@
-import React from "react"
+import React from "react";
 import { Link } from 'react-router-dom';
 import heroImage from '../../assets/hero-image.png';
+import heroImageMask from '../../assets/hero-image-mask.png';
 
-function Hero(props) {
-    return (
-        <section className='section-hero'>
-            <div className="hero">
-                <p className="hero__subtitle">Lennert Slabbinck</p>
-                <h1 className="hero__title">Web developer &amp; <br /> front-end designer</h1>
-                <p className="hero__info">
-                    I’m an Antwerp based developer, creating fresh and engaging products that help boost user-interaction. I love finding creative solutions and projects that leave a lasting impression.                </p>
-                <ul className="hero__nav">
-                    <li className="hero__nav__url">
-                        <Link to="/projects">
-                            Check out my projects
-                            https://codepen.io/njmcode/pen/XbdjQz
+export default class Hero extends React.Component {
+
+    onHover() {
+        console.log('get of me')
+    }
+
+    render() {
+        return (
+
+            <section className='section-hero'>
+                <div className="hero">
+                    <p className="hero__subtitle">Lennert Slabbinck</p>
+                    <h1 className="hero__title">Web developer &amp; <br /> front-end designer</h1>
+                    <p className="hero__info">
+                        I’m an Antwerp based developer, creating fresh and engaging products that help boost user-interaction. I love finding creative solutions and projects that leave a lasting impression.                </p>
+                    <ul className="hero__nav">
+                        <li className="hero__nav__url">
+                            <Link to="/projects">
+                                Check out my projects
                         </Link>
-                    </li>
-                    <li>
-                        <p>or</p>
-                    </li>
-                    <li className="hero__nav__url">
-                        <Link to="/projects">
-                            Read about me
+                        </li>
+                        <li>
+                            <p>or</p>
+                        </li>
+                        <li className="hero__nav__url">
+                            <Link to="/projects">
+                                Read about me
                         </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="image-wrapper">
-                <img src={heroImage} alt='flying robin' />
-            </div>
-        </section>
-    )
+                        </li>
+                    </ul>
+                </div>
+                <div className="tile">
+                    <figure className="tile__figure">
+                        <img src={heroImageMask} className='tile__image' alt='flying robin' />
+                        {/* <img src={heroImage} className='tile__image' alt='flying robin' /> */}
+                    </figure>
+                </div>
+            </section>
+        )
+    }
 }
-
-export default Hero;
--- src/components/Layout/Scroll.js
+++ src/components/Layout/Scroll.js
@@ -3,9 +3,9 @@ import React from "react";
 export default class Scroll extends React.Component {
     render() {
         return (
-            <a href="" class="scroll">
-                <div class="scroll__icon">
-                    <span class="scroll__icon__dot"></span>
+            <a href="/contact" className="scroll">
+                <div className="scroll__icon">
+                    <span className="scroll__icon__dot"></span>
                 </div>
             </a>
         );
